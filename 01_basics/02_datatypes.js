c430aebee6833cfa42347936e1931732f2c04856
prabhat mishra <115269832+Prabhatmishra7131@users.noreply.github.com>
2024-02-28 04:40:22 +0000

    added datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";  //use new js standards for entire js file
+
+// alert(3+3)  //We are usin node, not browser
+
+let name = "Prabhat"
+let age = 21
+let isLoggegIn = false
+let state;
+
+// number => 2 to the power 53
+//bigint 
+//string => ""
+//boolean =>  true/false
+//null => standalone value
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof name);
+console.log(typeof age)
+console.log(typeof isLoggegIn);
+console.log(typeof state)
+console.log(typeof undefined)
+console.log(typeof null)         //classic flaw Null considered as object
