ba6ddf00686c93dd5c0da068318ada4c44ac05a8
prabhat mishra <115269832+Prabhatmishra7131@users.noreply.github.com>
2024-02-28 04:54:07 +0000

    basics of conversion started

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "Prabhat"
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+//  true => 1, false => 0
+// undefines => NaN
+// string => NaN
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// let isLoggedInString = "prabhat"
+// let booleanIsLoggedInString = Boolean(isLoggedInString);
+// console.log(booleanIsLoggedInString);
+
+let isLoggedInString = ""
+let booleanIsLoggedInString = Boolean(isLoggedInString);
+console.log(booleanIsLoggedInString);
