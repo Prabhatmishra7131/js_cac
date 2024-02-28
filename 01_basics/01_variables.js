81321d6ae11c49d063c44324e5557a2c9687746d
prabhat mishra <115269832+Prabhatmishra7131@users.noreply.github.com>
2024-02-28 04:26:21 +0000

    started variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 270702
+let accountMail = "PrabhatmishrA7131@gmail.com"
+var accountPass = "TestINGpass"
+accountCity = "Delhi"
+
+// accountId = 27770   //re-assignment to CONST is not possible/allowed
+accountMail = "Onallhacks@hotmail.com"
+accountPass = "NewTEstpass"
+accountCity = "vizag"
+let accountStatus;
+
+// console.log(accountId);
+console.table([accountId,accountMail,accountPass,accountCity,accountStatus]);
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,2 @@
 console.log("I'm testing my node engines")
+console.log("Again testing for mods")
