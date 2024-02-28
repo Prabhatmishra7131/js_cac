620615c0e4832cf862c2a0a29524b8c1fb4285f5
prabhat mishra <115269832+Prabhatmishra7131@users.noreply.github.com>
2024-02-28 05:12:59 +0000

    some num to string and arithmtic conversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -31,3 +31,33 @@ let someNumber = 33
 let someNumberString = String(someNumber)
 console.log(someNumberString);
 console.log(typeof(someNumberString));
+
+
+
+// ******************** Operations ********************* //
+let value = 33
+let negValue = -value
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2/2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2%2)
+
+let str1 = "Hello "
+let str2 = "Prabhat"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2)
+
+// console.log(+true)
+// console.log(+"")
+
+let gameCounter = 0
+gameCounter++
+console.log(gameCounter)
