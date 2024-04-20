eac7338778a94b650bc0f1a2632d9aa77f796608
prabhat mishra <115269832+Prabhatmishra7131@users.noreply.github.com>
2024-04-20 08:37:13 +0000

    Completed While and Do while looops

---
+++ 05_iterations/02_while_and_do.js
@@ -0,0 +1,18 @@
+let index=0;
+while (index<10) {
+    // console.log(`Value of index is ${index}`);
+    index+=2;
+}
+
+let myArray = ['Weeknd','Bella','Chris Brown','J cole']
+let arr = 0
+while (arr<myArray.length) {
+    // console.log(`Artist at index ${arr} is- ${myArray[arr]}`);
+    arr++;
+}
+
+let score = 100;
+do {
+    console.log(`Score is ${score}`);
+    score--;
+} while (score>100);
